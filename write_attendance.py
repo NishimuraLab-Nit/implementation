02ebe911b96e40cfac7d10be88cb480ff9dd6372
NishimuraLab-Nit <157661463+NishimuraLab-Nit@users.noreply.github.com>
2025-01-19 15:29:02 +0900

    Update write_attendance.py

--- write_attendance.py
+++ write_attendance.py
@@ -114,7 +114,7 @@ def record_attendance(students_data, courses_data):
                     print(f"シート '{entry_month}' が見つかりません。スキップします。")
                     break
 
-                row = (course_id_int * 10) + entry_index
+                row = course_id_int + entry_index
                 column = entry_time.day + 1
                 sheet_to_update.update_cell(row, column, result)
                 print(f"出席記録: {course['class_name']} - {result}")
