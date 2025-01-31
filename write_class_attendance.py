fdf8454b4da6bf759f32af30d40ab13f875afdb9
NishimuraLab-Nit <157661463+NishimuraLab-Nit@users.noreply.github.com>
2025-01-31 11:14:55 +0900

    Update write_class_attendance.py

--- write_class_attendance.py
+++ write_class_attendance.py
@@ -197,8 +197,8 @@ def main(class_index="E5"):
             continue
 
         # entry{period} と exit{period} を確認
-        entry_key = f"entry{period}"
-        exit_key  = f"exit{period}"
+        entry_key = entry1
+        exit_key  = exit1
 
         if entry_key not in attendance_data:
             print(f"No {entry_key} found ⇒ skip.")
