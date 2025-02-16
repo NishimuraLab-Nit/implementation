7a7033e5083a2db9aa79947a7ced5c9e0f073b9e
NishimuraLab-Nit <157661463+NishimuraLab-Nit@users.noreply.github.com>
2025-02-16 14:43:15 +0900

    Update write_attendance.py

--- write_attendance.py
+++ write_attendance.py
@@ -146,6 +146,14 @@ def judge_attendance_for_period(entry_dt, exit_dt, start_dt, finish_dt):
     # 【修正】entry はあるが exit が無い場合
     # ---------------------------
     if entry_dt and (exit_dt is None):
+            # 遅刻 + 次コマへまたがる
+    if (
+        entry_dt
+        and exit_dt
+        and entry_dt >= (start_dt + td_5min)
+    ):
+        delta_min = int((entry_dt - start_dt).total_seconds() // 60)
+        return f"△遅{delta_min}分"
         # 今回の要件：entry のみのときは「○」にしつつ新たに exit や次コマは作らない
         return "〇", entry_dt, None, None
 
