494fd683bc4d77b1e6dc61f4a7bcb522746a56cb
NishimuraLab-Nit <157661463+NishimuraLab-Nit@users.noreply.github.com>
2025-02-16 14:25:23 +0900

    Update write_attendance.py

--- write_attendance.py
+++ write_attendance.py
@@ -14,7 +14,7 @@ if not firebase_admin._apps:
     firebase_admin.initialize_app(
         cred,
         {
-            "databaseURL": "https://test-xxxxx-default-rtdb.firebaseio.com/",  # ここは適宜書き換え
+            "databaseURL": "https://test-51ebc-default-rtdb.firebaseio.com/",  # ここは適宜書き換え
         },
     )
 else:
